b90c1131b7895f0116593b0238874d5d14831847
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-11-15 16:57:53 -0300

    Agrego archivos

---
+++ .vscode/peliculas.js
@@ -0,0 +1,98 @@
+
+
+
+const movies = [
+    {
+        id: 1,
+        title: "La princesa y el sapo",
+        rating: 7,
+        awards: 3,
+        length: 97,
+        price: 3000,
+        genre: "Animación"
+    },
+    {
+        id: 2,
+        title: "Harry Potter y el Cáliz de Fuego",
+        rating: 8,
+        awards: 4,
+        length: 157,
+        price: 8000,
+        genre: "Aventuras"
+    },
+    {
+        id: 3,
+        title: "Jurassic Park",
+        rating: 9,
+        awards: 5,
+        length: 127,
+        price: 7000,
+        genre: "Acción"
+    },
+    {
+        id: 4,
+        title: "Volver al Futuro",
+        rating: 8,
+        awards: 3,
+        length: 116,
+        price: 5000,
+        genre: "Aventuras"
+    },
+    {
+        id: 5,
+        title: "Hangover",
+        rating: 7,
+        awards: 1,
+        length: 100,
+        price: 3500,
+        genre: "Comedia"
+    },
+    {
+        id: 6,
+        title: "Hannibal",
+        rating: 7,
+        awards: 2,
+        length: 131,
+        price: 4500,
+        genre: "Suspenso"
+    },
+    {
+        id: 7,
+        title: "Indiana Jones",
+        rating: 8,
+        awards: 4,
+        length: 115,
+        price: 6000,
+        genre: "Aventuras"
+    },
+    {
+        id: 8,
+        title: "Matrix",
+        rating: 9,
+        awards: 5,
+        length: 136,
+        price: 10000,
+        genre: "Acción"
+    },
+    {
+        id: 9,
+        title: "Avatar",
+        rating: 8,
+        awards: 3,
+        length: 162,
+        price: 9500,
+        genre: "Aventuras"
+    },
+    {
+        id: 10,
+        title: "Toy Story",
+        rating: 8,
+        awards: 2,
+        length: 81,
+        price: 1500,
+        genre: "Animación"
+    }
+];
+
+
+module.exports = movies;
